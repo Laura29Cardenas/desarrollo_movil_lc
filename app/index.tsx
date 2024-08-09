4b4aa035faa7a4b0ba1e5c6ffc9b809821772567
Laura <lcardenasavila07@gmail.com>
2024-08-09 08:44:59 -0500

    commit inicial

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,39 @@
-import { Text, View } from "react-native";
+import { StyleSheet , Text, View } from 'react-native';
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Link } from 'expo-router';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
-export default function Index() {
+/*Opcion 1*/
+/* export default function App(){
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">Aora ADSO!</Text>
+      <StatusBar style='auto' />
+      <Link href="../profile" style={{ color: 'blue'}} > Go to profile </Link>
     </View>
   );
+} */
+
+/*Opcion 2*/
+export default function App() {
+  return (
+    <View style={style.container}>
+      <Text style={style.text} >Aora ADSO</Text>
+      <StatusBar style='auto' />
+      <Link href='/profile' style={{ color: 'blue' }}>Go to Profile</Link>
+    </View>
+  )
 }
+
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+  },
+})
